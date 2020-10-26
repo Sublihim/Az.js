3163e8b855cffe2c2cf329bdb2792740d76dbb4d
AndreyK <sublihim@ya.ru>
2020-10-26 13:30:59 +0300

    change build markdown task in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,37 @@
-var gulp = require('gulp'),
+const gulp = require('gulp'),
     gutil = require('gulp-util'),
     uglify = require('gulp-uglify'),
     concat = require('gulp-concat'),
     rename = require('gulp-rename'),
-    filesize = require('gulp-filesize'),
     sourcemaps = require('gulp-sourcemaps'),
-    jsdoc2md = require('gulp-jsdoc-to-markdown'),
-    merge = require('merge-stream'),
+    jsdoc2md = require('jsdoc-to-markdown'),
     fs = require('fs');
 
-gulp.task('docs', function () {
-  return merge(
-    gulp.src(['src/az.tokens.js', 'src/az.morph.js'])
-      .pipe(jsdoc2md({ template: fs.readFileSync('./api.hbs', 'utf8') }))
-      .on('error', function (err) {
-        gutil.log(gutil.colors.red('jsdoc2md failed'), err.message)
-      })
-      .pipe(rename(function (path) {
-        path.basename = path.basename.split('.').map(function(part) {
-          return part[0].toLocaleUpperCase() + part.substr(1);
-        }).join('.');
-        path.extname = '.md'
-      }))
-      .pipe(gulp.dest('wiki')),
-    gulp.src('README.md')
-      .pipe(rename(function (path) {
-        path.basename = 'Home';
-      }))
-      .pipe(gulp.dest('wiki'))
-  );
+gulp.task('docs', done => {
+    const jsdocOptions = {
+        files: ['src/az.tokens.js', 'src/az.morph.js'],
+        template: fs.readFileSync('./api.hbs', 'utf8'),
+        noCahe: true,
+    };
+    const output = jsdoc2md.renderSync(jsdocOptions);
+    fs.writeFileSync('./wiki/README.md', output);
+    done();
 });
 
-gulp.task('default', function() {
-  return gulp.src(['src/az.js', 'src/az.*.js'])
-    .pipe(sourcemaps.init())
-    .pipe(concat('az.js'))
-    .pipe(gulp.dest('dist'))
-    .pipe(uglify())
-    .pipe(rename('az.min.js'))
-    .pipe(sourcemaps.write('.', { includeContent: false, sourceRoot: '../src' }))
-    .pipe(gulp.dest('dist'))
-    .on('error', gutil.log);
+gulp.task('default', done => {
+    gulp.src(['src/az.js', 'src/az.*.js'])
+        .pipe(sourcemaps.init())
+        .pipe(concat('az.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(uglify())
+        .pipe(rename('az.min.js'))
+        .pipe(
+            sourcemaps.write('.', {
+                includeContent: false,
+                sourceRoot: '../src',
+            })
+        )
+        .pipe(gulp.dest('dist'))
+        .on('error', gutil.log);
+    done();
 });
