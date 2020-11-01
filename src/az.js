df0e14a90b9c2fe56324ed413a33cb08edaa2431
AndreyK <sublihim@ya.ru>
2020-11-02 01:55:10 +0300

    fix az namespace

--- src/az.js
+++ src/az.js
@@ -14,92 +14,92 @@ const ResponceType = {
     ARRAY_BUFFER: 'arraybuffer',
 };
 
-class Az {
-    /**
-     *
-     * @param {string} url
-     * @param {Az.ResponceType} responseType
-     * @param {Function} callback
-     */
-    static load(url, responseType, callback) {
-        if (fs) {
-            Az._loadFs(url, responseType, callback);
-        } else {
-            Az._loadRequest(url, responseType, callback);
-        }
-    }
-
-    /** */
-    static extend() {
-        const result = {};
-        for (let i = 0; i < arguments.length; i++) {
-            for (let key in arguments[i]) {
-                result[key] = arguments[i][key];
-            }
-        }
-        return result;
-    }
-
-    /**
-     *
-     * @param {string} url
-     * @param {Az.ResponceType} responseType
-     * @param {Function} callback
-     */
-    static _loadFs(url, responseType, callback) {
-        fs.readFile(
-            url,
-            {
-                encoding: responseType === Az.ResponceType.JSON ? 'utf8' : null,
-            },
-            (err, data) => {
-                if (err) {
-                    callback(err);
-                    return;
-                }
-
-                if (responseType === Az.ResponceType.JSON) {
-                    callback(null, JSON.parse(data));
-                } else if (responseType === Az.ResponceType.ARRAY_BUFFER) {
-                    if (data.buffer) {
-                        callback(null, data.buffer);
-                    } else {
-                        const ab = new ArrayBuffer(data.length);
-                        const view = new Uint8Array(ab);
-                        for (let i = 0; i < data.length; ++i) {
-                            view[i] = data[i];
-                        }
-                        callback(null, ab);
-                    }
-                } else {
-                    callback(new Error('Unknown responseType'));
-                }
-            }
-        );
-    }
-
-    /**
-     *
-     * @param {string} url
-     * @param {Az.ResponceType} responseType
-     * @param {Function} callback
-     */
-    static _loadRequest(url, responseType, callback) {
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', url, true);
-        xhr.responseType = responseType;
-
-        xhr.onload = function (e) {
-            if (xhr.response) {
-                callback && callback(null, xhr.response);
-            }
-        };
-
-        xhr.send(null);
+/**
+ *
+ * @param {string} url
+ * @param {Az.ResponceType} responseType
+ * @param {Function} callback
+ */
+function load(url, responseType, callback) {
+    if (fs) {
+        Az._loadFs(url, responseType, callback);
+    } else {
+        Az._loadRequest(url, responseType, callback);
     }
 }
 
-Az.ResponceType = ResponceType;
-Az.Tokens = require('./tokens/tokens');
+/** */
+function extend() {
+    const result = {};
+    for (let i = 0; i < arguments.length; i++) {
+        for (let key in arguments[i]) {
+            result[key] = arguments[i][key];
+        }
+    }
+    return result;
+}
 
-module.exports = Az;
+/**
+ *
+ * @param {string} url
+ * @param {Az.ResponceType} responseType
+ * @param {Function} callback
+ */
+function _loadFs(url, responseType, callback) {
+    fs.readFile(
+        url,
+        {
+            encoding: responseType === Az.ResponceType.JSON ? 'utf8' : null,
+        },
+        (err, data) => {
+            if (err) {
+                callback(err);
+                return;
+            }
+
+            if (responseType === Az.ResponceType.JSON) {
+                callback(null, JSON.parse(data));
+            } else if (responseType === Az.ResponceType.ARRAY_BUFFER) {
+                if (data.buffer) {
+                    callback(null, data.buffer);
+                } else {
+                    const ab = new ArrayBuffer(data.length);
+                    const view = new Uint8Array(ab);
+                    for (let i = 0; i < data.length; ++i) {
+                        view[i] = data[i];
+                    }
+                    callback(null, ab);
+                }
+            } else {
+                callback(new Error('Unknown responseType'));
+            }
+        }
+    );
+}
+
+/**
+ *
+ * @param {string} url
+ * @param {Az.ResponceType} responseType
+ * @param {Function} callback
+ */
+function _loadRequest(url, responseType, callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = responseType;
+
+    xhr.onload = function (e) {
+        if (xhr.response) {
+            callback && callback(null, xhr.response);
+        }
+    };
+
+    xhr.send(null);
+}
+
+module.exports = {
+    load: load,
+    extend: extend,
+    ResponceType: ResponceType,
+    Tokens: require('./tokens/tokens'),
+};
