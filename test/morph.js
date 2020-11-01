fb5e37171b188d4ec04b6856f949be3b31c59adc
AndreyK <sublihim@ya.ru>
2020-11-02 02:16:01 +0300

    change test

--- test/morph.js
+++ test/morph.js
@@ -1,14 +1,20 @@
 var expect = require('chai').expect;
 var Az = require('../dist/az');
 
-describe('Az.Morph', function() {
-  before(function(done) {
-    Az.Morph.init('dicts', done);
-  });
+describe('Az.Morph', function () {
+    before(function (done) {
+        Az.Morph.init('dicts', done);
+    });
 
-  it('should not parse latin words', function () {
-    var result = Az.Morph('word');
-    expect(result).to.be.an('array');
-    expect(result).to.have.length(0);
-  });
+    // it('should not parse latin words', function () {
+    //   var result = Az.Morph('word');
+    //   expect(result).to.be.an('array');
+    //   expect(result).to.have.length(0);
+    // });
+
+    it('can parse latin words', function () {
+        var result = Az.Morph('word');
+        expect(result).to.be.an('array');
+        expect(result).to.have.length(1);
+    });
 });
